bc11c6b75ce3e9ef287d827a74cb320a199b5cd7
Stephen Denekamp <me@stephendenekamp.dev>
2020-12-05 14:37:04 +0000

    Initial Commit

---
+++ Shared/My_AudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_AudioApp.swift
+//  Shared
+//
+//  Created by Stephen Denekamp on 05/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct My_AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
